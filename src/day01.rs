fb69055ec3a89ece507ac1de28320e6c7be8768d
Evan Wildenhain <evan.wildenhain@gmail.com>
2022-12-02 16:58:08 -0500

    2022

--- src/day01.rs
+++ src/day01.rs
@@ -1,40 +1,53 @@
+use itertools::Itertools as _;
+use std::iter::Iterator as _;
+
 #[aoc_generator(day1)]
-pub fn parse(input: &str) -> Vec<i32> {
-  input.lines()
-    .map(|line| line.parse::<i32>().unwrap())
-    .collect()
+pub fn parse(input: &str) -> Vec<Vec<i32>> {
+    input
+        .lines()
+        .group_by(|line| *line == "")
+        .into_iter()
+        .filter_map(|(k, group)| {
+            (!k).then(|| group.map(|s| s.parse::<i32>().unwrap()).collect::<Vec<_>>())
+        })
+        .collect()
 }
 
 #[aoc(day1, part1)]
-fn pt1(inp: &Vec<i32>) -> i32 {
-  inp.iter().fold((None, 0), |(prev, n), curr| {
-    (Some(curr), match prev {
-      None => n,
-      Some(p) => if curr > p { n + 1 } else { n }
-    })
-  }).1
+fn pt1(inp: &Vec<Vec<i32>>) -> i32 {
+    inp.iter().map(|elf| elf.iter().sum()).max().unwrap()
 }
 
 #[aoc(day1, part2)]
-fn pt2(inp: &Vec<i32>) -> i32 {
-  use std::collections::VecDeque;
-  let mut deq: VecDeque<i32> = VecDeque::new();
-  let mut num_increases = 0;
-  let mut curr_sum = None;
-  for i in inp.iter() {
-    deq.push_back(*i);
-    if deq.len() == 4 {
-      deq.pop_front();
-    }
-    if deq.len() == 3 {
-      let new_sum = deq.iter().sum::<i32>();
-      if let Some(curr) = curr_sum{ 
-        if new_sum > curr {
-          num_increases += 1;
-        }
-      }
-      curr_sum = Some(new_sum);
-    }
-  }
-  num_increases
+fn pt2(inp: &Vec<Vec<i32>>) -> i32 {
+    inp.iter()
+        .map(|elf| elf.iter().sum::<i32>())
+        .sorted()
+        .rev()
+        .take(3)
+        .sum::<i32>()
 }
+
+// #[aoc(day1, part2)]
+// fn pt2(inp: &Vec<Vec<i32>>) -> i32 {
+//     use std::collections::VecDeque;
+//     let mut deq: VecDeque<i32> = VecDeque::new();
+//     let mut num_increases = 0;
+//     let mut curr_sum = None;
+//     for i in inp.iter() {
+//         deq.push_back(*i);
+//         if deq.len() == 4 {
+//             deq.pop_front();
+//         }
+//         if deq.len() == 3 {
+//             let new_sum = deq.iter().sum::<i32>();
+//             if let Some(curr) = curr_sum {
+//                 if new_sum > curr {
+//                     num_increases += 1;
+//                 }
+//             }
+//             curr_sum = Some(new_sum);
+//         }
+//     }
+//     num_increases
+// }
